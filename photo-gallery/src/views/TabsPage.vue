2d3f897fd0667b9a98e141beb556527b281d8d46
BLACK079 <148823174+BLACK079@users.noreply.github.com>
2023-11-22 16:45:46 +0000

    Camara

--- photo-gallery/src/views/TabsPage.vue
+++ photo-gallery/src/views/TabsPage.vue
@@ -7,11 +7,13 @@
           <ion-icon aria-hidden="true" :icon="triangle" />
           <ion-label>Tab 1</ion-label>
         </ion-tab-button>
-
+        
         <ion-tab-button tab="tab2" href="/tabs/tab2">
-          <ion-icon aria-hidden="true" :icon="ellipse" />
-          <ion-label>Tab 2</ion-label>
+        <ion-icon :icon="images" />
+        <ion-label>Photos</ion-label>
         </ion-tab-button>
+
+
 
         <ion-tab-button tab="tab3" href="/tabs/tab3">
           <ion-icon aria-hidden="true" :icon="square" />
@@ -24,5 +26,5 @@
 
 <script setup lang="ts">
 import { IonTabBar, IonTabButton, IonTabs, IonLabel, IonIcon, IonPage, IonRouterOutlet } from '@ionic/vue';
-import { ellipse, square, triangle } from 'ionicons/icons';
+import { images, square, triangle } from 'ionicons/icons';
 </script>
