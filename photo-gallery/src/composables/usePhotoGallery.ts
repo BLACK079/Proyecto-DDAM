017aaa3db59f41d5129b246e65f0fb4ab8d10c6f
BLACK079 <148823174+BLACK079@users.noreply.github.com>
2023-11-28 03:17:11 +0000

    Borrando Fotos

--- photo-gallery/src/composables/usePhotoGallery.ts
+++ photo-gallery/src/composables/usePhotoGallery.ts
@@ -3,6 +3,7 @@ import { Camera, CameraResultType, CameraSource, Photo } from '@capacitor/camera
 import { Filesystem, Directory } from '@capacitor/filesystem';
 import { Preferences } from '@capacitor/preferences';
 
+
 const photos = ref<UserPhoto[]>([]);
 
 export const usePhotoGallery = () => {
@@ -13,18 +14,18 @@ export const usePhotoGallery = () => {
           quality: 100,
         });
       
-      const fileName = Date.now() + '.jpeg';
-const savedFileImage = {
-  filepath: fileName,
-  webviewPath: photo.webPath,
-};
-
-photos.value = [savedFileImage, ...photos.value];
-    };
+        const fileName = Date.now() + '.jpeg';
+        const savedFileImage = await savePicture(photo, fileName);
+      
+        photos.value = [savedFileImage, ...photos.value];
+      };
+      
+    
   
     return {
-        photos,
+     photos,
       takePhoto,
+      deletePhoto,
     };
   };
 
@@ -32,7 +33,6 @@ photos.value = [savedFileImage, ...photos.value];
     filepath: string;
     webviewPath?: string;
   }
-
   const convertBlobToBase64 = (blob: Blob) =>
   new Promise((resolve, reject) => {
     const reader = new FileReader();
@@ -42,66 +42,61 @@ photos.value = [savedFileImage, ...photos.value];
     };
     reader.readAsDataURL(blob);
   });
- 
+
   const savePicture = async (photo: Photo, fileName: string): Promise<UserPhoto> => {
-    let base64Data: string;
-    // "hybrid" will detect mobile - iOS or Android
-    if (isPlatform('hybrid')) {
-      const file = await Filesystem.readFile({
-        path: photo.path!,
-      });
-      base64Data = file.data;
-    } else {
-      // Fetch the photo, read as a blob, then convert to base64 format
-      const response = await fetch(photo.webPath!);
-      const blob = await response.blob();
-      base64Data = (await convertBlobToBase64(blob)) as string;
-    }
+    // Fetch the photo, read as a blob, then convert to base64 format
+    const response = await fetch(photo.webPath!);
+    const blob = await response.blob();
+    const base64Data = (await convertBlobToBase64(blob)) as string;
+  
     const savedFile = await Filesystem.writeFile({
       path: fileName,
       data: base64Data,
       directory: Directory.Data,
     });
   
-    if (isPlatform('hybrid')) {
-      // Display the new image by rewriting the 'file://' path to HTTP
-      // Details: https://ionicframework.com/docs/building/webview#file-protocol
-      return {
-        filepath: savedFile.uri,
-        webviewPath: Capacitor.convertFileSrc(savedFile.uri),
-      };
-    } else {
-      // Use webPath to display the new image instead of base64 since it's
-      // already loaded into memory
-      return {
-        filepath: fileName,
-        webviewPath: photo.webPath,
-      };
-    }
+    // Use webPath to display the new image instead of base64 since it's
+    // already loaded into memory
+    console.log(fileName);
+    return {
+      filepath: fileName,
+      webviewPath: photo.webPath,
+    };
   };
-  const PHOTO_STORAGE = 'photos'
+
+  const PHOTO_STORAGE = 'photos';
   const cachePhotos = () => {
     Preferences.set({
       key: PHOTO_STORAGE,
       value: JSON.stringify(photos.value),
     });
   };
+
   watch(photos, cachePhotos);
 
   const loadSaved = async () => {
     const photoList = await Preferences.get({ key: PHOTO_STORAGE });
-  const photosInPreferences = photoList.value ? JSON.parse(photoList.value) : [];
+    const photosInPreferences = photoList.value ? JSON.parse(photoList.value) : [];
   
-  if (!isPlatform('hybrid')) {
     for (const photo of photosInPreferences) {
       const file = await Filesystem.readFile({
         path: photo.filepath,
         directory: Directory.Data,
       });
-  onMounted(loadSaved);
-  photo.webviewPath = `data:image/jpeg;base64,${file.data}`;
-}
-}
-
-photos.value = photosInPreferences;
-};
+      photo.webviewPath = `data:image/jpeg;base64,${file.data}`;
+    }
+  
+    photos.value = photosInPreferences;
+  };
+  
+  const deletePhoto = async (photo: UserPhoto) => {
+    // Remove this photo from the Photos reference data array
+    photos.value = photos.value.filter((p) => p.filepath !== photo.filepath);
+  
+    // delete photo file from filesystem
+    const filename = photo.filepath.substr(photo.filepath.lastIndexOf('/') + 1);
+    await Filesystem.deleteFile({
+      path: filename,
+      directory: Directory.Data,
+    });
+  };
--- photo-gallery/src/views/Tab2Page.vue
+++ photo-gallery/src/views/Tab2Page.vue
@@ -16,19 +16,18 @@
   <ion-grid>
     <ion-row>
       <ion-col size="6" :key="photo.filepath" v-for="photo in photos">
-        <ion-img :src="photo.webviewPath"></ion-img>
+       <ion-img :src="photo.webviewPath" @click="showActionSheet(photo)"></ion-img>
       </ion-col>
     </ion-row>
   </ion-grid>
-
   <!-- <ion-fab> markup  -->
 </ion-content>
   </ion-page>
-
 </template>
 
 <script setup lang="ts">
 import {
+  actionSheetController,
   IonPage,
   IonHeader,
   IonFab,
@@ -37,13 +36,39 @@ import {
   IonToolbar,
   IonTitle,
   IonContent,
+  IonImg,
   IonGrid,
   IonRow,
   IonCol,
-  IonImg,
 } from '@ionic/vue';
-import {camera, trash, close } from 'ionicons/icons';
+// other imports
+import { camera, trash, close } from 'ionicons/icons';
 import { usePhotoGallery, UserPhoto } from '@/composables/usePhotoGallery';
-const {photos, takePhoto } = usePhotoGallery();
 
+const { photos, takePhoto, deletePhoto } = usePhotoGallery();
+
+const showActionSheet = async (photo: UserPhoto) => {
+  const actionSheet = await actionSheetController.create({
+    header: 'Photos',
+    buttons: [
+      {
+        text: 'Delete',
+        role: 'destructive',
+        icon: trash,
+        handler: () => {
+          deletePhoto(photo);
+        },
+      },
+      {
+        text: 'Cancel',
+        icon: close,
+        role: 'cancel',
+        handler: () => {
+          // Nothing to do, action sheet is automatically closed
+        },
+      },
+    ],
+  });
+  await actionSheet.present();
+};
 </script>
