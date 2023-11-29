14e17923cd3000b5f263951aa8d1353562fc8d6e
BLACK079 <148823174+BLACK079@users.noreply.github.com>
2023-11-29 15:32:00 +0000

    Resultado Final ✌

--- photo-gallery/src/views/Tab2Page.vue
+++ photo-gallery/src/views/Tab2Page.vue
@@ -1,3 +1,4 @@
+
 <template>
   <ion-page>
     <ion-header>
@@ -16,7 +17,7 @@
   <ion-grid>
     <ion-row>
       <ion-col size="6" :key="photo.filepath" v-for="photo in photos">
-       <ion-img :src="photo.webviewPath" @click="showActionSheet(photo)"></ion-img>
+        <ion-img :src="photo.webviewPath" @click="showActionSheet(photo)"></ion-img>
       </ion-col>
     </ion-row>
   </ion-grid>
@@ -24,7 +25,6 @@
 </ion-content>
   </ion-page>
 </template>
-
 <script setup lang="ts">
 import {
   actionSheetController,
@@ -43,10 +43,8 @@ import {
 } from '@ionic/vue';
 // other imports
 import { camera, trash, close } from 'ionicons/icons';
-import { usePhotoGallery, UserPhoto } from '@/composables/usePhotoGallery';
-
+import { usePhotoGallery, UserPhoto  } from '@/composables/usePhotoGallery';
 const { photos, takePhoto, deletePhoto } = usePhotoGallery();
-
 const showActionSheet = async (photo: UserPhoto) => {
   const actionSheet = await actionSheetController.create({
     header: 'Photos',
