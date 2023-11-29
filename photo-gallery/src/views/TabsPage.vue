6ce6b4313559f34b35421caebb8ede532986a5e5
BLACK079 <148823174+BLACK079@users.noreply.github.com>
2023-11-29 15:42:29 +0000

    Borrando el triangulo ✔

--- photo-gallery/src/views/TabsPage.vue
+++ photo-gallery/src/views/TabsPage.vue
@@ -3,10 +3,7 @@
     <ion-tabs>
       <ion-router-outlet></ion-router-outlet>
       <ion-tab-bar slot="bottom">
-        <ion-tab-button tab="tab1" href="/tabs/tab1">
-          <ion-icon aria-hidden="true" :icon="triangle" />
-          <ion-label>Inicio</ion-label>
-        </ion-tab-button>
+        
         
         <ion-tab-button tab="tab2" href="/tabs/tab2">
         <ion-icon :icon="images" />
@@ -23,5 +20,5 @@
 
 <script setup lang="ts">
 import { IonTabBar, IonTabButton, IonTabs, IonLabel, IonIcon, IonPage, IonRouterOutlet } from '@ionic/vue';
-import { images, triangle } from 'ionicons/icons';
+import { images} from 'ionicons/icons';
 </script>
